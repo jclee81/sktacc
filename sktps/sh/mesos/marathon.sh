19888576775aa3ed1c22d8c22ed380f5926f7bd2
Hong Seok Hwan <sipqko@gmail.com>
2017-08-06 11:46:49 +0900

    Add mesos & marathon run script.

---
+++ sktps/sh/mesos/marathon.sh
@@ -0,0 +1,1 @@
+marathon-1.4.5/bin/start --master zk://127.0.0.1:2181/sktps001 --zk zk://127.0.0.1:2181/marathon
---
+++ sktps/sh/mesos/slave.sh
@@ -0,0 +1,2 @@
+rm -f /Users/shhong/mesos_work_dir/meta/slaves/latest
+/usr/local/sbin/mesos-slave --master=zk://127.0.0.1:2181/sktps001 --work_dir=/Users/shhong/mesos_work_dir/
